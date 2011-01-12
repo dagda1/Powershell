b01b8f9a31fbe8779e97607281e1b5c401ca2202
dagda1 <dagda1@scotalt.net>
2011-01-12 15:06:10 +0000

    readme added

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,11 @@
 set-alias notepad "C:\Program Files (x86)\Notepad++\notepad++.exe"
 
-function gototrunk()
-{
-	set-location C:\projects\ncontinuity2\trunk
-}
+function gototrunk{set-location C:\projects\ncontinuity2\trunk}
 
-function gotoprojects()
-{
-	set-location C:\projects
-}
+function gotoprojects{set-location C:\projects}
+
+function gotoprofile{set-location C:\Users\paul.cowan\Documents\WindowsPowerShell}
 
 set-alias tr gototrunk
 set-alias pr gotoprojects
+set-alias profile gotoprofile
