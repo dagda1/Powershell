8564ba35f85234bb2ded3a3516c1f9e5153c682f
dagda1 <dagda1@scotalt.net>
2011-01-12 15:02:23 +0000

    Powershell profile added

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+set-alias notepad "C:\Program Files (x86)\Notepad++\notepad++.exe"
+
+function gototrunk()
+{
+	set-location C:\projects\ncontinuity2\trunk
+}
+
+function gotoprojects()
+{
+	set-location C:\projects
+}
+
+set-alias tr gototrunk
+set-alias pr gotoprojects
