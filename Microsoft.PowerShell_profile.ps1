b331804511858deaa7418752fd9adb0b893ecbc3
dagda1 <dagda1@scotalt.net>
2011-02-01 09:57:37 +0000

    backup.ps1 created

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,6 +7,21 @@ function gotoprojects{set-location C:\projects}
 
 function gotoprofile{set-location C:\Users\paul.cowan\Documents\WindowsPowerShell}
 
+function gotobuild{set-location C:\projects\ncontinuity2\trunk\build\net-4.0\debug}
+
+function gotodownloads{set-location C:\Users\paul.cowan\Downloads}
+
+function gotoncontinuity2{set-location c:\projects\ncontinuity2}
+
+function gotolead{set-location c:\projects\leadcapturer}
+
+function gotocurrent{set-location "C:\projects\ncontinuity2\branches\2.4.0"}
+
 set-alias tr gototrunk
 set-alias pr gotoprojects
 set-alias profile gotoprofile
+set-alias build gotobuild
+set-alias downloads gotodownloads
+set-alias nc gotoncontinuity2
+set-alias lead gotolead
+set-alias live gotocurrent
