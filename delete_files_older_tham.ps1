d351ab9d321432dbb2809cf541a8370853ef4028
paul.cowan <paul.cowan@continuity2.com>
2011-10-11 11:26:26 +0100

    delete older than update

--- delete_files_older_tham.ps1
+++ delete_files_older_tham.ps1
@@ -1,9 +1,12 @@
-﻿$Now = Get-Date
-$TargetFolder = $args[0]
-$Files = ls $TargetFolder -Include *.bak -Recurse | where {$_.LastWriteTime -le "Get-Date.AddDays(-7)"}
-
-foreach($File in $Files)
-{
-	Write-Host "Deleting FIle $File" -ForegroundColor "Red";
-	Remove-Item $File | Out-Null
-}
+﻿$cutoff = (Get-Date) - (New-TimeSpan -Days 7)
+ 
+$before = (Get-ChildItem $args[0] -Recurse | Measure-Object Length -Sum).Sum
+ 
+Get-ChildItem $args[0] -Recurse |                           
+  Where-Object { $_.Length -ne $null } |            
+  Where-Object { $_.LastWriteTime -lt $cutoff } |   
+  Remove-Item -Force -ErrorAction SilentlyContinue -WhatIf  # REMOVE -whatif to ENABLE DELETING!
+ 
+$after = (Get-ChildItem $args[0] -Recurse | Measure-Object Length -Sum).Sum
+ 
+'Freed {0:0.00} MB disk space' -f (($before-$after)/1MB)
