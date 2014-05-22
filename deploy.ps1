c44f4ece1e27e5ea8fc337bed7ff56cf09128b06
Paul <dagda1@scotalt.net>
2014-05-22 21:58:33 +0100

    remove

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,3 @@
-$pass = convertto-securestring "0c3anrac5" -asplaintext -force
-$mycred = new-object -typename System.Management.Automation.PSCredential -argumentlist "CT2-WS1\CT2.Admin",$pass
+$pass = convertto-securestring "" -asplaintext -force
+$mycred = new-object -typename System.Management.Automation.PSCredential -argumentlist "",$pass
 invoke-command -filepath C:\projects\Powershell\backup.ps1 -computerName 89.251.114.100  -Credential $mycred
